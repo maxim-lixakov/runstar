7b0c40aeb9248cc5ceaae3b33b795f22f5243501
mvliksako1 <mvliksako1@mts.ru>
2023-04-20 21:58:22 +0300

    обновлена таблица, добавлен поиск по фамилии, имени

--- probeg/main/views.py
+++ probeg/main/views.py
@@ -6,9 +6,17 @@ from .models import DjResult
 
 def index(request):
     item_list = DjResult.objects.all()
+
+    # Get the search term from the request GET parameters
+    search_term = request.GET.get('q')
+
+    if search_term:
+        item_list = item_list.filter(lname__icontains=search_term) | \
+                item_list.filter(fname__icontains=search_term)
+
     paginator = Paginator(item_list, 25)  # Show 25 items per page
 
     page = request.GET.get('page')
     items = paginator.get_page(page)
 
-    return render(request, "table.html", {'items': items})
+    return render(request, "table.html", {'items': items, 'search_term': search_term})
