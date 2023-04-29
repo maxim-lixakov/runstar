a9c615e1a9d8432000f981d42644ed1d01294c52
mvliksako1 <mvliksako1@mts.ru>
2023-04-30 00:50:19 +0300

    добавлена интеграция личного кабинета с wagtail cms

--- probeg/main/authed_views.py
+++ probeg/main/authed_views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.contrib.auth.models import Group, User
 from django.views.generic import CreateView
+from wagtail.models import Page
 
 from .models import DjResult, DjRunner
 from .forms import CreationForm
@@ -13,11 +15,20 @@ class SignUp(CreateView):
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
 
+    def form_valid(self, form):
+        form.instance.save()
+        group = Group.objects.get(name="Users")
+        group.user_set.add(form.instance)
+        return super(CreateView, self).form_valid(form)
+
+
 
 # @login_required
 def profile(request):
     uid = request.GET.get('id', 0)
+    current_user = request.user
 
     runner = DjRunner.objects.filter(id=uid).first()
-
-    return render(request, 'profile.html', {'runner': runner})
+    pages = Page.objects.filter(owner_id=current_user.id).all()
+    print([page.url_path for page in pages])
+    return render(request, 'profile.html', {'runner': runner, 'pages': pages})
