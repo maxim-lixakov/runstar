9b22d156733c380faafb9d252712196aed4b3f18
mvliksako1 <mvliksako1@mts.ru>
2023-05-03 01:58:21 +0300

    поправлена разметка

--- probeg/main/authed_views.py
+++ probeg/main/authed_views.py
@@ -35,32 +35,35 @@ def profile(request):
     uid = request.GET.get('id', 0)
     runner = DjRunner.objects.filter(id=uid).first()
 
-    # Get the best results for all distances for the current runner
-    results = DjResult.objects.filter(runner=runner.id).order_by('race__dist', '-time_raw').distinct()
+    if runner:
 
-    # Create a dictionary to store the best results by distance
-    best_results = {}
-    for result in results:
-        if float(result.race.dist) < 43:
-            if result.race.dist in best_results:
-                best_results[float(result.race.dist)].append(result.time_raw)
-            else:
-                best_results[float(result.race.dist)] = [result.time_raw]
+        # Get the best results for all distances for the current runner
+        results = DjResult.objects.filter(runner=runner.id).order_by('race__dist', '-time_raw').distinct()
 
-    # Get a list of all the distances in the best_results dictionary
-    distances = list(best_results.keys())
+        # Create a dictionary to store the best results by distance
+        best_results = {}
+        for result in results:
+            if float(result.race.dist) < 43:
+                if result.race.dist in best_results:
+                    best_results[float(result.race.dist)].append(result.time_raw)
+                else:
+                    best_results[float(result.race.dist)] = [result.time_raw]
 
-    # Render the updated profile page template with the runner and results data
-    pbs = []
-    for res in best_results:
-        pb = BestResult(res, best_results[res][-1])
-        pbs.append(pb)
+        # Get a list of all the distances in the best_results dictionary
+        distances = list(best_results.keys())
 
-    context = {
-        'runner': runner,
-        'distances': distances,
-        'best_results': pbs,
-        'n': len(distances),
-    }
-    print(context)
-    return render(request, 'profile.html', context)
+        # Render the updated profile page template with the runner and results data
+        pbs = []
+        for res in best_results:
+            pb = BestResult(res, best_results[res][-1])
+            pbs.append(pb)
+
+        context = {
+            'runner': runner,
+            'distances': distances,
+            'best_results': pbs,
+            'n': len(distances),
+        }
+        return render(request, 'profile.html', context)
+    else:
+        return render(request, 'profile.html', {})
